f65c9157876eaa87d927967022525b3eb75407c8
Crimethink13 <m.connley@live.com.au>
2022-06-05 23:34:04 +1000

    Added DOM manip and scoring

--- rps.js
+++ rps.js
@@ -1,14 +1,14 @@
-console.log('Lets Play Rock - Paper - Scissors!');
-
 //? Initialize variables
 const options = [ 'rock', 'paper', 'scissors' ];
 let playerChoice;
 let cpuChoice;
+let cpuScore = 0;
+let playerScore = 0;
 
 const cpuSelection = () => {
 	return options[Math.floor(Math.random() * options.length)];
 };
-
+//TODO Add a stop after 5 games
 const buttons = document.querySelectorAll('.btn');
 buttons.forEach((button) => {
 	button.addEventListener('click', function() {
@@ -17,69 +17,47 @@ buttons.forEach((button) => {
 
 		//? Run the game
 		function playGame() {
-			let cpuScore = 0;
-			let playerScore = 0;
-			let proceed = 0;
-
-			// let playerChoice = button.id;
 			let cpuChoice = cpuSelection();
+			let choices = playerChoice + ' ' + cpuChoice;
 
-			//? Determine outcome of one round
-			function playRound(playerChoice, cpuChoice) {
-				let choices = playerChoice + ' ' + cpuChoice;
-				switch (choices) {
-					case 'rock scissors':
-					case 'paper rock':
-					case 'scissors paper':
-						playerScore++;
-						return 'You Win!';
+			switch (choices) {
+				//? Case for player
+				case 'rock scissors':
+				case 'paper rock':
+				case 'scissors paper':
+					playerScore++;
+					break;
+				//? Case for CPU
+				case 'scissors rock':
+				case 'rock paper':
+				case 'paper scissors':
+					cpuScore++;
+					break;
 
-					case 'scissors rock':
-					case 'rock paper':
-					case 'paper scissors':
-						cpuScore++;
-						return 'You Lose!';
+				case 'rock rock':
+				case 'paper paper':
+				case 'scissors scissors':
+					break;
 
-					case 'rock rock':
-					case 'paper paper':
-					case 'scissors scissors':
-						return 'Tie Game!';
-
-					default:
-						'Invalid choice';
-				}
+				default:
+					'Invalid choice';
 			}
 
-			const results = document.querySelector('.results');
-			results.textContent = `You picked: ${playerChoice} || CPU picked: ${cpuChoice}`;
+			const player = document.querySelector('.player');
+			const computer = document.querySelector('.computer');
+			const winner = document.querySelector('.winner');
 
-			//? Print results of one round
-			let winner = playRound(playerChoice, cpuChoice);
-			console.log('----------------------------------');
-			console.log(winner);
-			console.log(`You picked: ${playerChoice} || CPU picked: ${cpuChoice}`);
-			console.log(`Your score: ${playerScore} || CPU score: ${cpuScore}`);
-			proceed--;
+			player.textContent = `${playerChoice} \n`;
+			player.textContent += `${playerScore}`;
 
-			//? 5 game loop finishes here
+			computer.textContent = `${cpuChoice} \n`;
+			computer.textContent += `${cpuScore}`;
 
-			//? Print final results of 5 rounds
-			console.log('----------------------------------');
-			console.log(`FINAL SCORES - You: ${playerScore} || CPU: ${cpuScore}`);
-
-			if (playerScore > cpuScore) {
-				console.log(`Nice! ${playerScore} games won!`);
-				return 'You win!';
-			} else if (playerScore < cpuScore) {
-				console.log(`Bad luck! CPU won ${cpuScore} games!`);
-				return 'You lose!';
-			} else if (playerScore === cpuScore) {
-				console.log(`You are both evenly matched with ${playerScore} games tied!`);
-				return 'Tie game!';
+			if (playerScore === 5) {
+				winner.textContent = 'You Win!';
+			} else if (cpuScore === 5) {
+				winner.textContent = 'You Lose!';
 			}
-			console.log('Enter "playRound()" to continue!');
 		}
 	});
 });
-
-playGame();
